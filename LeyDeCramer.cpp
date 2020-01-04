c33d095dcc03b5195babc604b14dfeb33db86346
TheusZer0 <44948678+TheusZer0@users.noreply.github.com>
2020-01-04 19:08:01 -0300

    Initial commit

---
+++ LeyDeCramer.cpp
@@ -0,0 +1,72 @@
+//
+// Created by TheusZero on 04-01-2020.
+//
+
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+
+//Declaraciones de Funciones
+
+//Funciones para Resolver un sistema 2X2
+void DetSistemaDos (float x, float y,float r,float X, float Y,float R);
+void DetXDos(float x, float y,float r,float X, float Y,float R);
+void DetYDos(float x, float y,float r,float X, float Y,float R);
+void DivCoefDos(float determinanteX, float determinanteSistema);
+void Imprimir(float Resultado);
+
+
+int main() {
+    float x;
+    float y;
+    float r;
+    float X;
+    float Y;
+    float R;
+
+    return 0;
+}
+
+void DetSistemaDos (float x, float y,float r,float X, float Y,float R){
+    //Determinante Sistema.
+    float uno[2] = {x,y};
+    float dos[2] = {X,Y};
+    float semiUno = uno[0] * dos[1];
+    float semiDos = uno[1] * dos[0];
+    float determinanteSistema = semiUno - semiDos;
+    cout << determinanteSistema;
+}
+
+void DetXDos(float x, float y,float r,float X, float Y,float R){
+    //Determinante de la X.
+    float uno[2] = {r,y};
+    float dos[2] = {R,Y};
+    float semiUno = uno[0] * dos[1];
+    float semiDos = uno[1] * dos[0];
+    float determinanteX = semiUno - semiDos;
+    cout << determinanteX;
+}
+
+void DetYDos(float x, float y,float r,float X, float Y,float R){
+    //Determinante de la Y.
+    float uno[2] = {x,r};
+    float dos[2] = {X,R};
+    float semiUno = uno[0] * dos[1];
+    float semiDos = uno[1] * dos[0];
+    float determianteY = semiUno - semiDos;
+    cout << determianteY;
+}
+
+void DivCoefDos(float determinanteX, float determinanteSistema){
+    //Division de los Coeficientes
+    float ValorX = (determinanteX/determinanteSistema);
+    float ValorY = (determinanteX/determinanteSistema);
+    float Resultado[2] = {ValorX,ValorY};
+    cout << ValorX << "," << ValorY;
+}
+
+void Imprimir(float Resultado){
+    cout << "el programa ha terminando de operar, los resultados fueron:"<< endl;
+    
+}
