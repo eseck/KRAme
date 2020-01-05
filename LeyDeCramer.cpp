b7235dc3574b3addadc1fdf0db3c83d8cbb52ce0
TheusZer0 <44948678+TheusZer0@users.noreply.github.com>
2020-01-04 21:25:06 -0300

    Punteros y grandes cambios uwu

--- LeyDeCramer.cpp
+++ LeyDeCramer.cpp
@@ -16,9 +16,6 @@ typedef struct dosXdos{
     float X;
     float Y;
     float R;
-    float determinanteSistema;
-    float determinanteX;
-    float determianteY;
     float Resultado;
 }D;
 
@@ -26,81 +23,58 @@ typedef struct dosXdos{
 
 //Funciones para Resolver un sistema 2X2
 void Insertar(D* X);
-void DetSistemaDos (float x, float y,float r,float X, float Y,float R); //Determinante Sistema.
-void DetXDos(float x, float y,float r,float X, float Y,float R); //Determinante de la X.
-void DetYDos(float x, float y,float r,float X, float Y,float R); //Determinante de la Y.
-void DivCoefDos(float determinanteX, float determinanteSistema); //Division de los Coeficientes
+void Desarrollo(D X);
 void ImprimirResultados(float Resultado);
 
 
 int main() {
+    D* ejercicio = new D;
+    Insertar(ejercicio);
+    Desarrollo(D);
+    delete(ejercicio);
     return 0;
 }
 
 void Insertar(D* Ejercicio){
     float x,y,r,X,Y,R;
-    cout << "Ingrese el valor de X para la primera ecuacion";
+    cout << "Ingrese el valor que acompañara al X para la primera ecuacion";
     cin >> x;
-    cout << "Ingrese el valor de Y para la primera ecuacion";
+    cout << "Ingrese el valor que acompañara al Y para la primera ecuacion";
     cin >> y;
     cout << "Ingrese el valor de la ecuacion";
     cin >> r;
-    cout << "Ingrese el valor de X para la segunda ecuacion";
+    cout << "Ingrese el valor que acompañara al X para la segunda ecuacion";
     cin >> X;
-    cout << "Ingrese el valor de Y para la segunda ecuacion";
+    cout << "Ingrese el valor que acompañara al Y para la segunda ecuacion";
     cin >> Y;
     cout << "Ingrese el valor de la segunda ecuacion";
     cin >> R;
-
-
-
-    /*
-    cout <<"Nombre: ";
-    cin>>name;cout<<"Rut:";
-    cin>>rut;cout<<"Numero: ";
-    cin>>number;cout<< "Edad: ";
-    cin>>edad;X->name=name;
-    X->edad=edad;
-    X->number=number;
-    X->rut=rut;*/
+    Ejercicio->x=x;
+    Ejercicio->y=y;
+    Ejercicio->r=r;
+    Ejercicio->X=X;
+    Ejercicio->Y=Y;
+    Ejercicio->R=R;
 }
 
-void DetSistemaDos (float x, float y,float r,float X, float Y,float R){
+void Desarrollo(D Ejercicio){
+    //arrays para ordenar los valores
+    float uno[3] = {Ejercicio.x,Ejercicio.y,Ejercicio.r};
+    float dos[3] = {Ejercicio.X,Ejercicio.Y,Ejercicio.R};
     //Determinante Sistema.
-    float uno[2] = {x,y};
-    float dos[2] = {X,Y};
-    float semiUno = uno[0] * dos[1];
-    float semiDos = uno[1] * dos[0];
-    float determinanteSistema = semiUno - semiDos;
-    cout << determinanteSistema;
-}
-
-void DetXDos(float x, float y,float r,float X, float Y,float R){
+    float determinanteSistema = (uno[0] * dos[1])-(uno[1] * dos[0]);
+    cout<<determinanteSistema;
     //Determinante de la X.
-    float uno[2] = {r,y};
-    float dos[2] = {R,Y};
-    float semiUno = uno[0] * dos[1];
-    float semiDos = uno[1] * dos[0];
-    float determinanteX = semiUno - semiDos;
-    cout << determinanteX;
-}
-
-void DetYDos(float x, float y,float r,float X, float Y,float R){
+    float determinanteX = (uno[3] * dos[1])-(uno[3] * dos[0]);
+    cout<<determinanteX;
     //Determinante de la Y.
-    float uno[2] = {x,r};
-    float dos[2] = {X,R};
-    float semiUno = uno[0] * dos[1];
-    float semiDos = uno[1] * dos[0];
-    float determianteY = semiUno - semiDos;
-    cout << determianteY;
-}
-
-void DivCoefDos(float determinanteX, float determinanteSistema){
-    //Division de los Coeficientes
+    float determianteY = (uno[0] * dos[3]) - (uno[3] * dos[0]);
+    cout<<determianteY;
+    /*Division de los Coeficientes
     float ValorX = (determinanteX/determinanteSistema);
     float ValorY = (determinanteX/determinanteSistema);
     float Resultado[2] = {ValorX,ValorY};
-    cout << ValorX << "," << ValorY;
+    cout << ValorX << "," << ValorY;*/
 }
 
 void ImprimirResultados(float Resultado){
@@ -108,5 +82,3 @@ void ImprimirResultados(float Resultado){
     cout << "(" << Resultado << "," << ")" << endl;
     cout << "Gracias por preferir NeoTel Companny" << endl;
 }
-//wena wena geente//
-//wewen wen wue wueeeee buan buan buooooo tutarariruta tutararitibirurirubi//
