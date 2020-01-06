386b82c62268625887b1fc4efeb8794ab6da79c2
TheusZer0 <44948678+TheusZer0@users.noreply.github.com>
2020-01-05 22:13:13 -0300

    arreglo de identaciones

--- LeyDeCramer.cpp
+++ LeyDeCramer.cpp
@@ -20,6 +20,7 @@ typedef struct tresXtres{
 typedef struct cuatroXcuatro{
     float x,y,z,w,r,X,Y,Z,W,R,aX,aY,aZ,aW,aR,ax,ay,az,aw,ar;
     float DetSistema;
+    float DetX;
 }C;
 //Declaraciones de Funciones
 //Funciones para Resolver un sistema 2x2
@@ -32,6 +33,7 @@ void DesarrolloTres(T* X);
 void InsertarCuatro(C* X);
 void DesarrolloCuatroSistema(C* X);
 void DesarrolloCuatroX(C* Ejercicio);
+void DesarrolloCuatroY(C* Ejercicio);
 //Main
 // cuando colocamos un string en el main o las funciones insertar,se ejecuta una sola linea de cout infinitamente
 int main() {
@@ -60,6 +62,7 @@ int main() {
                 InsertarCuatro(ejercicioCuatro);
                 DesarrolloCuatroSistema(ejercicioCuatro);
                 DesarrolloCuatroX(ejercicioCuatro);
+                DesarrolloCuatroY(ejercicioCuatro);
                 delete(ejercicioDos);
                 break;
 
@@ -132,7 +135,6 @@ void DesarrolloDos(D* Ejercicio) {
         cout << endl << endl;
     }
 }
-
 //Funciones3X3
 void InsertarTres(T* Ejercicio) {
     float x,y,z,r,X,Y,Z,R,aX,aY,aZ,aR;
@@ -264,22 +266,40 @@ void DesarrolloCuatroX(C* Ejercicio){
     float UnoFilaDos[5] = {Ejercicio->aY,Ejercicio->aZ,Ejercicio->aW,Ejercicio->aY,Ejercicio->aZ};
     float UnoFilaTres[5] = {Ejercicio->ay,Ejercicio->az,Ejercicio->aw,Ejercicio->ay,Ejercicio->az};
     float UnoDiagonal= ((UnoFilaUno[0]*UnoFilaDos[1]*UnoFilaTres[2])+(UnoFilaUno[1]*UnoFilaDos[2]*UnoFilaTres[3])+(UnoFilaUno[2]*UnoFilaDos[3]*UnoFilaTres[4])) - ((UnoFilaUno[4]*UnoFilaDos[3]*UnoFilaTres[2])+(UnoFilaUno[3]*UnoFilaDos[2]*UnoFilaTres[1])+(UnoFilaUno[2]*UnoFilaDos[1]*UnoFilaTres[0]));
-
     float DosFilaUno[5] = {Ejercicio->R,Ejercicio->Z,Ejercicio->W,Ejercicio->R,Ejercicio->Z};
     float DosFilaDos[5] = {Ejercicio->aR,Ejercicio->aZ,Ejercicio->aW,Ejercicio->aR,Ejercicio->aZ};
     float DosFilaTres[5] = {Ejercicio->ar,Ejercicio->az,Ejercicio->aw,Ejercicio->ar,Ejercicio->az};
     float DosDiagonal= ((DosFilaUno[0]*DosFilaDos[1]*DosFilaTres[2])+(DosFilaUno[1]*DosFilaDos[2]*DosFilaTres[3])+(DosFilaUno[2]*DosFilaDos[3]*DosFilaTres[4])) - ((DosFilaUno[4]*DosFilaDos[3]*DosFilaTres[2])+(DosFilaUno[3]*DosFilaDos[2]*DosFilaTres[1])+(DosFilaUno[2]*DosFilaDos[1]*DosFilaTres[0]));
-
     float TresFilaUno[5] = {Ejercicio->R,Ejercicio->Y,Ejercicio->W,Ejercicio->R,Ejercicio->Y};
     float TresFilaDos[5] = {Ejercicio->aR,Ejercicio->aY,Ejercicio->aW,Ejercicio->aR,Ejercicio->aY};
     float TresFilaTres[5] = {Ejercicio->ar,Ejercicio->ay,Ejercicio->aw,Ejercicio->ar,Ejercicio->ay};
     float TresDiagonal= ((TresFilaUno[0]*TresFilaDos[1]*TresFilaTres[2])+(TresFilaUno[1]*TresFilaDos[2]*TresFilaTres[3])+(TresFilaUno[2]*TresFilaDos[3]*TresFilaTres[4])) - ((TresFilaUno[4]*TresFilaDos[3]*TresFilaTres[2])+(TresFilaUno[3]*TresFilaDos[2]*TresFilaTres[1])+(TresFilaUno[2]*TresFilaDos[1]*TresFilaTres[0]));
-
     float CuatroFilaUno[5] = {Ejercicio->R,Ejercicio->Y,Ejercicio->Z,Ejercicio->R,Ejercicio->Y};
     float CuatroFilaDos[5] = {Ejercicio->aR,Ejercicio->aY,Ejercicio->aZ,Ejercicio->aR,Ejercicio->aY};
     float CuatroFilaTres[5] = {Ejercicio->ar,Ejercicio->ay,Ejercicio->az,Ejercicio->ar,Ejercicio->ay};
     float CuatroDiagonal= ((CuatroFilaUno[0]*CuatroFilaDos[1]*CuatroFilaTres[2])+(CuatroFilaUno[1]*CuatroFilaDos[2]*CuatroFilaTres[3])+(CuatroFilaUno[2]*CuatroFilaDos[3]*CuatroFilaTres[4])) - ((CuatroFilaUno[4]*CuatroFilaDos[3]*CuatroFilaTres[2])+(CuatroFilaUno[3]*CuatroFilaDos[2]*CuatroFilaTres[1])+(CuatroFilaUno[2]*CuatroFilaDos[1]*CuatroFilaTres[0]));
-
     float ResultadoX = ((FilaUno[0]*UnoDiagonal)+(FilaUno[1]*DosDiagonal*-1)+(FilaUno[2]*TresDiagonal*1)+(FilaUno[3]*CuatroDiagonal*-1));
     cout<<"El determinante de X es: "<< ResultadoX<<endl;
+    Ejercicio->DetX=ResultadoX;
+}
+void DesarrolloCuatroY(C* Ejercicio){
+    float FilaUno[4] = {Ejercicio->x,Ejercicio->r,Ejercicio->z,Ejercicio->w};
+    float UnoFilaUno[5] = {Ejercicio->R,Ejercicio->Z,Ejercicio->W,Ejercicio->R,Ejercicio->Z};
+    float UnoFilaDos[5] = {Ejercicio->aR,Ejercicio->aZ,Ejercicio->aW,Ejercicio->aR,Ejercicio->aZ};
+    float UnoFilaTres[5] = {Ejercicio->ar,Ejercicio->az,Ejercicio->aw,Ejercicio->ar,Ejercicio->az};
+    float UnoDiagonal= ((UnoFilaUno[0]*UnoFilaDos[1]*UnoFilaTres[2])+(UnoFilaUno[1]*UnoFilaDos[2]*UnoFilaTres[3])+(UnoFilaUno[2]*UnoFilaDos[3]*UnoFilaTres[4])) - ((UnoFilaUno[4]*UnoFilaDos[3]*UnoFilaTres[2])+(UnoFilaUno[3]*UnoFilaDos[2]*UnoFilaTres[1])+(UnoFilaUno[2]*UnoFilaDos[1]*UnoFilaTres[0]));
+    float DosFilaUno[5] = {Ejercicio->X,Ejercicio->Z,Ejercicio->W,Ejercicio->X,Ejercicio->Z};
+    float DosFilaDos[5] = {Ejercicio->aX,Ejercicio->aZ,Ejercicio->aW,Ejercicio->aX,Ejercicio->aZ};
+    float DosFilaTres[5] = {Ejercicio->ax,Ejercicio->az,Ejercicio->aw,Ejercicio->ax,Ejercicio->az};
+    float DosDiagonal= ((DosFilaUno[0]*DosFilaDos[1]*DosFilaTres[2])+(DosFilaUno[1]*DosFilaDos[2]*DosFilaTres[3])+(DosFilaUno[2]*DosFilaDos[3]*DosFilaTres[4])) - ((DosFilaUno[4]*DosFilaDos[3]*DosFilaTres[2])+(DosFilaUno[3]*DosFilaDos[2]*DosFilaTres[1])+(DosFilaUno[2]*DosFilaDos[1]*DosFilaTres[0]));
+    float TresFilaUno[5] = {Ejercicio->X,Ejercicio->R,Ejercicio->W,Ejercicio->X,Ejercicio->R};
+    float TresFilaDos[5] = {Ejercicio->aX,Ejercicio->aR,Ejercicio->aW,Ejercicio->aX,Ejercicio->aR};
+    float TresFilaTres[5] = {Ejercicio->ax,Ejercicio->ar,Ejercicio->aw,Ejercicio->ax,Ejercicio->ar};
+    float TresDiagonal= ((TresFilaUno[0]*TresFilaDos[1]*TresFilaTres[2])+(TresFilaUno[1]*TresFilaDos[2]*TresFilaTres[3])+(TresFilaUno[2]*TresFilaDos[3]*TresFilaTres[4])) - ((TresFilaUno[4]*TresFilaDos[3]*TresFilaTres[2])+(TresFilaUno[3]*TresFilaDos[2]*TresFilaTres[1])+(TresFilaUno[2]*TresFilaDos[1]*TresFilaTres[0]));
+    float CuatroFilaUno[5] = {Ejercicio->X,Ejercicio->R,Ejercicio->Z,Ejercicio->X,Ejercicio->R};
+    float CuatroFilaDos[5] = {Ejercicio->aX,Ejercicio->aR,Ejercicio->aZ,Ejercicio->aX,Ejercicio->aR};
+    float CuatroFilaTres[5] = {Ejercicio->ax,Ejercicio->ar,Ejercicio->az,Ejercicio->ax,Ejercicio->ar};
+    float CuatroDiagonal= ((CuatroFilaUno[0]*CuatroFilaDos[1]*CuatroFilaTres[2])+(CuatroFilaUno[1]*CuatroFilaDos[2]*CuatroFilaTres[3])+(CuatroFilaUno[2]*CuatroFilaDos[3]*CuatroFilaTres[4])) - ((CuatroFilaUno[4]*CuatroFilaDos[3]*CuatroFilaTres[2])+(CuatroFilaUno[3]*CuatroFilaDos[2]*CuatroFilaTres[1])+(CuatroFilaUno[2]*CuatroFilaDos[1]*CuatroFilaTres[0]));
+    float ResultadoY = ((FilaUno[0]*UnoDiagonal)+(FilaUno[1]*DosDiagonal*-1)+(FilaUno[2]*TresDiagonal*1)+(FilaUno[3]*CuatroDiagonal*-1));
+    cout<<"El Determinante de Y es: "<<ResultadoY<<endl;
 }
